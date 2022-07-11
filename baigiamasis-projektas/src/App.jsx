52a63f315b91175406473803b46b002237e6e3d4
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-11 16:25:46 +0300

    Nomralize  App with header

---
+++ baigiamasis-projektas/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/JS/header'
+import QuestionsComp from './components/JS/Questions';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <QuestionsComp />
+    </div>
+  );
+}
+
+export default App;
