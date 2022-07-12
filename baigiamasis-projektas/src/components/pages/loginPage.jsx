bbfc6e16e09bb9bc4137e042a4678d655cf2d7ec
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-12 21:48:57 +0300

    login reg pages + css

---
+++ baigiamasis-projektas/src/components/pages/loginPage.jsx
@@ -0,0 +1,49 @@
+import { useNavigate } from 'react-router-dom'
+import '../CSS/LoginRegPage.css'
+
+const LoginForm = () => {
+
+    const redirect = useNavigate()
+
+    const LoginForm = (e) => {
+        e.preventDefault()
+        const login = {
+            email: e.target.elements.email.value,
+            password: e.target.elements.password.value
+        }
+        e.target.elements.email.value = ''
+        e.target.elements.password.value = ''
+        fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(login),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.err) {
+                    return alert(data.err)
+                }
+                else {
+                    localStorage.setItem('token', data.token)
+                    redirect('/Home')
+                }
+            })
+            .catch(err => { return alert(err) })
+
+    }
+
+    return (
+        <form onSubmit={LoginForm} className="LogNRegForm">
+            <h1>Login</h1>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" minLength={8} title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required  />
+            <input type="submit" value={"Login"} />
+        </form>
+    );
+}
+
+export default LoginForm;
---
+++ baigiamasis-projektas/src/components/pages/register.jsx
@@ -0,0 +1,47 @@
+import { useNavigate } from 'react-router-dom'
+import '../CSS/LoginRegPage.css'
+
+const RegisterForm = () => {
+    const redirect = useNavigate()
+
+
+    const RegisterForm = (e) => {
+        e.preventDefault()
+        const login = {
+            email: e.target.elements.email.value,
+            password: e.target.elements.password.value
+        }
+        e.target.elements.email.value = ''
+        e.target.elements.password.value = ''
+        fetch('http://localhost:5000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(login),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.changes !== 1) return alert('Email or password is not valid')
+                else {
+                    alert('Registration succesfull, you will be redirected to Login window soon')
+                    setTimeout(() => redirect('/login'), 1000)
+                };
+            })
+            .catch(err => { return alert(err) })
+
+    }
+
+    return (
+        <form onSubmit={RegisterForm} className="LogNRegForm">
+            <h1>Register</h1>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" required />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" minLength={8} title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+            <input type="submit" value={"Register"} />
+        </form>
+    );
+}
+
+export default RegisterForm;
