04a36ac5d6d66bb7211256c919ce7619b31968d4
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-12 11:01:23 +0300

    Added basic routing

--- baigiamasis-projektas/src/App.jsx
+++ baigiamasis-projektas/src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+//Componenets
 import Header from './components/JS/header'
-import QuestionsComp from './components/JS/Questions';
+import Footer from './components/JS/footer'
+//pages
+import AskAQuestion from '../src/components/pages/createQuestionPage'
+import HomePage from '../src/components/pages/home'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
-      <QuestionsComp />
+      <Routes>
+        <Route path='/' element={<HomePage /> } />
+        <Route path="/askquestion" element={<AskAQuestion />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
