13b8b7d3ee16b7788ebd392d70253d9df17fc65f
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-12 11:00:42 +0300

    footer added

---
+++ baigiamasis-projektas/src/components/JS/footer.jsx
@@ -0,0 +1,11 @@
+import '../CSS/footer.css'
+
+const PageFooter = () => {
+    return (
+        <footer>
+            <h4 >Copyright © 2022 Rokas Skara. All rights reserved.</h4>
+        </footer>
+    );
+}
+
+export default PageFooter;
