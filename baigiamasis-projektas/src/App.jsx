63bd9b5a95d1ccb193cb1d6a5561b002457cf800
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-12 21:49:44 +0300

    appjs routes updated

--- baigiamasis-projektas/src/App.jsx
+++ baigiamasis-projektas/src/App.jsx
@@ -1,22 +1,36 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
+import { createContext, useState } from 'react'
 //Componenets
 import Header from './components/JS/header'
 import Footer from './components/JS/footer'
 //pages
 import AskAQuestion from '../src/components/pages/createQuestionPage'
 import HomePage from '../src/components/pages/home'
+import UserContext from './components/JS/userContext';
+import LoginForm from './components/pages/loginPage';
+import RegisterForm from './components/pages/register';
+
+
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
   return (
     <div className='App'>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage /> } />
-        <Route path="/askquestion" element={<AskAQuestion />} />
-      </Routes>
-      <Footer />
-    </div>
+      <UserContext.Provider value={user} >
+        <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/register' element={<RegisterForm />} />
+          <Route path='/askquestion' element={<AskAQuestion />} />
+        </Routes>
+        <Footer />
+      </UserContext.Provider>
+    </div >
+
   );
 }
 
