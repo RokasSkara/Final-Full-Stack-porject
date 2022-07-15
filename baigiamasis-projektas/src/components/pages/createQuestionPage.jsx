ac2e5c08961efa729efbdb88277683fb0af9a647
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-15 16:21:35 +0300

    create question only for logged in users

--- baigiamasis-projektas/src/components/pages/createQuestionPage.jsx
+++ baigiamasis-projektas/src/components/pages/createQuestionPage.jsx
@@ -1,16 +1,23 @@
 import '../CSS/createQuestionPage.css'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import { useState } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import UserContext from '../JS/userContext';
+import LoginForm from '../pages/loginPage';
 
 
 
 const CreateQuestionPage = () => {
 
     const redirect = useNavigate()
+
+    const logged = useContext(UserContext)
+
+     useEffect(() => {
+        logged.checkAuth()
+    }, []) 
 
     const submitQuestion = (e) => {
         e.preventDefault()
@@ -25,60 +32,73 @@ const CreateQuestionPage = () => {
                 content: e.target.elements.content.value
             }),
         })
-        .then(res => res.json())
-        .then(data => {
-            if(data.error){
-                return alert(data.error)
-            } else {
-                alert('Posted Successfully!')
-                e.target.elements.title.value = ''
-                e.target.elements.content.value = ''
-                setquestionTitle('')
-                setquestionBody('')
-                setTimeout(() => {
-                    redirect('/')
-                }, 2000);
-            }}
+            .then(res => res.json())
+            .then(data => {
+                if (data.err) {
+                    logged.checkAuth()
+                    return alert(data.err)
+                } else {
+                    alert('Posted Successfully!')
+                    e.target.elements.title.value = ''
+                    e.target.elements.content.value = ''
+                    setquestionTitle('')
+                    setquestionBody('')
+                    setTimeout(() => {
+                        redirect('/')
+                    }, 2000);
+                }
+            }
             )
-            
-        .catch(err => alert(err))
+
+            .catch(err => err === undefined? alert('Access Denied, relog and try again') : alert(err))
     }
 
 
     const [questionTitle, setquestionTitle] = useState('')
     const [questionBody, setquestionBody] = useState('')
 
-    return (
-        <section className="askAQuestionForm">
-            <h1>Ask a question</h1>
-            <form onSubmit={submitQuestion}>
+    return (<>
+        {logged.user !== null ?
+            <section className="askAQuestionForm">
 
-                <input
-                    type="text" name="title" placeholder="Enter question title"
-                    value={questionTitle}
-                    onChange={(e) => setquestionTitle(e.target.value)}
-                />
+                <h1>Ask a question</h1>
+                <form onSubmit={submitQuestion}>
 
-                <textarea
-                    name="content" cols="30" rows="10"
-                    onChange={(e) => setquestionBody(e.target.value)}
-                    defaultValue = {questionBody}
-                >
-                    
-                </textarea>
+                    <input
+                        type="text" name="title" placeholder="Enter question title"
+                        value={questionTitle}
+                        onChange={(e) => setquestionTitle(e.target.value)}
+                    />
 
-                <div className='PreviewArea'>
-                    <div className='PreviewAreaTitle'>
-                        <ReactMarkdown Plugins={[remarkGfm]} children={questionTitle} />
+                    <textarea
+                        name="content" cols="30" rows="10"
+                        onChange={(e) => setquestionBody(e.target.value)}
+                        defaultValue={questionBody}
+                    >
+
+                    </textarea>
+
+                    <div className='PreviewArea'>
+                        <div className='PreviewAreaTitle'>
+                            <ReactMarkdown Plugins={[remarkGfm]} children={questionTitle} />
+                        </div>
+                        <div className='PreviewAreaBody'>
+                            <ReactMarkdown Plugins={[remarkGfm]} children={questionBody} />
+                        </div>
                     </div>
-                    <div className='PreviewAreaBody'>
-                        <ReactMarkdown Plugins={[remarkGfm]} children={questionBody} />
-                    </div>
-                </div>
-                <input type="Submit"/>
-            </form>
+                    <input type="Submit" />
+                </form>
 
-        </section>);
+            </section>
+            :
+            <> 
+            <h1 className='PostQuestionErrMsg'>User needs to be logged in</h1>
+            <LoginForm />
+            </>
+            }
+            
+
+    </>);
 }
 
 export default CreateQuestionPage;
