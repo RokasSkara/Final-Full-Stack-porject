3d9668a8c1f18557f3fcf3073b06c05677086bd7
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-19 20:27:30 +0300

    answer/question delete/update

--- baigiamasis-projektas/server/routes/api/Answers.js
+++ baigiamasis-projektas/server/routes/api/Answers.js
@@ -4,15 +4,28 @@ import fetch from 'node-fetch'
 
 const router = express.Router()
 
+
+const QuestionAddAnswer = (id) => {
+    fetch(`http://localhost:8080/Posts/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            fetch(`http://localhost:8080/Posts/${id}`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    answer: data.answer + 1
+                })
+            })
+        })
+
+}
+
 router.get('/:id?', (req, res) => {
-    console.log(req.params.id)
     if (req.params.id) {
         fetch(`http://localhost:8080/Answers`)
             .then(res => res.json())
             .then(data => {
-
-                let data2 = data.filter(item => item.questionID === req.params.id)
-                console.log(data2)
+                const data2 = data.filter(item => item.questionID === req.params.id)
                 res.send(data2)
             })
             .catch(err => res.send(err))
@@ -30,7 +43,7 @@ router.post('/', isAuth, (req, res) => {
     const UserId = req.cookies.token?.split('+')[2]
     const user = req.cookies.token?.split('+')[1]
 
-     if (questionID && answer) {
+    if (questionID && answer) {
         fetch('http://localhost:8080/Answers', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -40,16 +53,60 @@ router.post('/', isAuth, (req, res) => {
                 answerContent: answer,
                 questionID: questionID,
                 answerTime: new Date().toLocaleString(),
+                eddited: false,
+                edditDate: null,
+                votes: 0
             }),
         })
             .then(res => res.json())
             .then(data => {
+                QuestionAddAnswer(questionID)
                 res.status(200).send({ msg: "All good!" })
             })
             .catch(err => res.status(400).send({ err: err }))
     } else {
         return res.send({ err: 'Content missing' })
-    } 
+    }
+})
+
+router.delete('/delete/:id', isAuth, (req, res) => {
+    const { id } = req.params
+
+    fetch(`http://localhost:8080/Answers/${id}`, {
+        method: 'DELETE',
+    })
+        .then(res => res.json())
+        .then(data => {
+            res.status(200).send({ msg: "Answer Deleted" })
+        })
+        .catch(err => res.status(400).send({ err: err }))
+}
+)
+
+router.patch('/:id', isAuth, (req, res) => {
+    console.log('inside')
+    const { id } = req.params
+    const { content } = req.body
+    if (content.length >= 1) {
+
+        fetch(`http://localhost:8080/Answers/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                answerContent: req.body.content,
+                eddited: true,
+                edditDate: new Date().toLocaleString()
+
+            }),
+        })
+            .then(res => res.json())
+            .then(data => res.send({ msg: 'Data updated' }))
+    } else {
+        res.send({ msg: 'Content too short, if you want to remove the comment select Delete' })
+    }
+
 })
 
 export default router;
--- baigiamasis-projektas/server/routes/api/Question.js
+++ baigiamasis-projektas/server/routes/api/Question.js
@@ -4,6 +4,24 @@ import fetch from 'node-fetch'
 
 const router = express.Router()
 
+
+const deleteAnswers = (id) => {
+    let temp = []
+    fetch(`http://localhost:8080/Answers`)
+        .then(res => res.json())
+        .then(data => {
+            data.filter(item => item.questionID === id ? temp.push(item.id) : '')
+            if (temp.length > 0) {
+                temp.map(item => {
+                    fetch(`http://localhost:8080/Answers/${item}`, {
+                        method: 'DELETE',
+                    })
+                })
+            }
+        })
+}
+
+
 router.get('/:id?', (req, res) => {
     if (req.params.id) {
         fetch(`http://localhost:8080/Posts/${req.params.id}`)
@@ -17,8 +35,6 @@ router.get('/:id?', (req, res) => {
             .catch(err => res.send(err))
     }
 })
-
-
 
 
 router.post('/', isAuth, (req, res) => {
@@ -40,11 +56,11 @@ router.post('/', isAuth, (req, res) => {
                 postStatus: {
                     eddited: false,
                     edditDate: null,
-                    answered: false,
-                    votes: 0,
-                    answers: 0,
-                    views: 0
-                }
+                },
+                answered: false,
+                votes: 0,
+                answer: 0,
+                views: 0
             }),
         })
             .then(res => res.json())
@@ -54,6 +70,83 @@ router.post('/', isAuth, (req, res) => {
             .catch(err => res.status(400).send({ err: err }))
     } else {
         return res.send({ err: 'Title or Content missing' })
+    }
+})
+
+router.delete('/delete/:id', isAuth, (req, res) => {
+    const { id } = req.params
+
+    fetch(`http://localhost:8080/Posts/${id}`, {
+        method: 'DELETE',
+    })
+        .then(res => res.json())
+        .then(data => {
+            res.status(200).send({ msg: "Post Deleted" })
+            deleteAnswers(id)
+        })
+        .catch(err => res.status(400).send({ err: err }))
+}
+
+)
+
+router.patch('/:id', isAuth, (req, res) => {
+    const { title, content } = req.body
+    console.log(title.length)
+    if (title && content && title.length > 1 && content.length > 1) {
+        fetch(`http://localhost:8080/Posts/${req.params.id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                title: title,
+                content: content,
+                postTime: new Date().toLocaleString(),
+                postStatus: {
+                    eddited: true,
+                    edditDate: new Date().toLocaleString(),
+                }
+            }),
+        })
+            .then(res => res.json())
+            .then(data => {
+                res.status(200).send({ msg: "Question updated!" })
+            })
+            .catch(err => res.status(400).send({ err: err }))
+    } else {
+        return res.send({ msg: 'Title or Content missing' })
+    }
+})
+
+router.patch('/answered/:id', isAuth, (req, res) => {
+    fetch(`http://localhost:8080/Posts/${req.params.id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            answered: true,
+        }),
+    })
+        .then(res => res.json())
+        .then(data => {
+            res.status(200).send({ msg: "Question updated!" })
+        })
+        .catch(err => res.status(400).send({ err: err }))
+
+})
+
+router.patch('/views/:id', (req, res) => {
+    const { views } = req.body
+    if (views) {
+        fetch(`http://localhost:8080/Posts/${req.params.id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                views: views,
+            }),
+        })
+            .then(res => res.json())
+            .then(data => {
+                res.status(200).send({ msg: "Question updated!" })
+            })
+            .catch(err => res.status(400).send({ err: err }))
     }
 })
 
