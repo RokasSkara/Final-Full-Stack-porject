0a660b1c141e24fd76b4f24b4f217a782d60b011
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-19 20:29:41 +0300

    delete answer

--- baigiamasis-projektas/src/components/pages/answers.jsx
+++ baigiamasis-projektas/src/components/pages/answers.jsx
@@ -1,15 +1,38 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import '../CSS/answers.css'
-const Answer = ({ props }) => {
+import UserContext from '../JS/userContext';
+import VotingButtons from "../JS/votingButtons";
+
+import { useContext, useState } from 'react';
+import EditAnswerForm from "../JS/editAnswerForm";
+
+
+const Answer = ({ props, getAnswers}) => {
+    const [EditAnswer, setEditAnswer] = useState(false)
+
+    const DeleteAnswer = (e) => {
+        fetch(`http://localhost:5000/answers/delete/${props.id}`, {
+            method: 'DELETE',
+            credentials: 'include'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data) {
+                    getAnswers()
+                    alert(data.msg)
+                }
+            })
+    }
+
+    const logged = useContext(UserContext)
+
     return (<>
         <div className="Answer-Wrapper">
             <section className="FullAnswer">
                 <div className="viewQuesitonStats">
                     <div className="VoteArrows">
-                        <button>&#9650;</button>
-                        <div>0</div>
-                        <button>&#9660;</button>
+                        <VotingButtons votes={props.votes}/>
                     </div>
                 </div>
                 <div className="ShowAnswer">
@@ -17,8 +40,25 @@ const Answer = ({ props }) => {
                 </div>
             </section>
             <div className="AnswerUserInfo">
-                <div className="WhoWhenAnswered">{`posted on  ${props.answerTime} by ${props.answerOwner} `}</div>
+                <div>
+                    {logged.user ?
+                        <>
+                            {props.answerOwner === logged.user.username ?
+                                <>
+                                    <button className="AddAnAnswer" onClick={() => { if (window.confirm('Are you sure you wish to edit the answer?')) { EditAnswer === false ? setEditAnswer(true) : setEditAnswer(false) } }} >Edit</button>
+                                    <button className="AddAnAnswer" onClick={() => { if (window.confirm('Are you sure you wish to delete this answer?')) { DeleteAnswer() } }}>Delete</button>
+                                </>
+                                : ''}</>
+                        :
+                        ''}
+                </div>
+                <div className="WhoWhenAnswered">{props.eddited === false ? `posted on  ${props.answerTime} by ${props.answerOwner} ` : `eddited on ${props.edditDate} by ${props.answerOwner}`}</div>
             </div>
+        </div>
+        <div>
+            {EditAnswer &&
+                <EditAnswerForm props={props} getAnswers={getAnswers} setEditAnswer={setEditAnswer} />
+            }
         </div>
     </>);
 }
